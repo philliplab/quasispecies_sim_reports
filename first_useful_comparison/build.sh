694e3e0a263a34db81ba0dd482cdad531567ce71
Phillip Labuschagne <philliplab@gmail.com>
2018-09-18 08:36:19 +0200

    GET rid of that final figures folder

--- first_useful_comparison/build.sh
+++ first_useful_comparison/build.sh
@@ -1,3 +1,4 @@
 rm _main.Rmd
 R -e "bookdown::preview_chapter('first_useful_comparison.Rmd', output_dir = '/home/phillipl/projects/quasispecies_sim_reports/builds/first_useful_comparison')"
 firefox /home/phillipl/projects/quasispecies_sim_reports/builds/first_useful_comparison/simulation-description.html
+rm -r _bookdown_files
