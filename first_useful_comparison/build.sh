95843beb80c0dd659a7896479d7b2d01425e9d53
Phillip Labuschagne <philliplab@gmail.com>
2018-09-18 09:52:41 +0200

    TWEAK heading names

--- first_useful_comparison/build.sh
+++ first_useful_comparison/build.sh
@@ -1,4 +1,4 @@
 rm _main.Rmd
 R -e "bookdown::preview_chapter('first_useful_comparison.Rmd', output_dir = '/home/phillipl/projects/quasispecies_sim_reports/builds/first_useful_comparison')"
-firefox /home/phillipl/projects/quasispecies_sim_reports/builds/first_useful_comparison/simulation-description.html
+firefox /home/phillipl/projects/quasispecies_sim_reports/builds/first_useful_comparison/first-useful-comparison.html
 rm -r _bookdown_files
