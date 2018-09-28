80585a4d49fb1f0d15063edeacd266691c6a4339
Phillip Labuschagne <philliplab@gmail.com>
2018-09-28 07:24:11 +0200

    Fix report headings, minor write-up improvements

--- basic_homolo_based_fitness/build.sh
+++ basic_homolo_based_fitness/build.sh
@@ -1,4 +1,4 @@
 rm _main.Rmd
-R -e "bookdown::preview_chapter('basic_homolo_based_fitness.Rmd', output_dir = '/home/phillipl/projects/quasispecies_sim_reports/builds/basic_homolo_based_fitness')"
-firefox /home/phillipl/projects/quasispecies_sim_reports/builds/basic_homolo_based_fitness/basic-homology-based-fitness.html
+R -e "rmarkdown::render('basic_homolo_based_fitness.Rmd', output_dir = '/home/phillipl/projects/quasispecies_sim_reports/builds/basic_homolo_based_fitness')"
+firefox /home/phillipl/projects/quasispecies_sim_reports/builds/basic_homolo_based_fitness/basic_homolo_based_fitness.html
 rm -r _bookdown_files
