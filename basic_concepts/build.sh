89e5e0397ef76f0dbc56a961367ee480376a31fd
Phillip Labuschagne <philliplab@gmail.com>
2018-09-28 13:33:42 +0200

    migrate basic_concepts report to the build script

---
+++ basic_concepts/build.sh
@@ -0,0 +1,2 @@
+R -e "rmarkdown::render('basic_concepts.Rmd', output_dir = '/home/phillipl/projects/quasispecies_sim_reports/builds/basic_concepts')"
+firefox /home/phillipl/projects/quasispecies_sim_reports/builds/basic_concepts/basic_concepts.html
