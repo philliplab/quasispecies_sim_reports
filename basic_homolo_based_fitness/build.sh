bdbe68af79e9823dbd2bb157dc66abe8381bd8fd
Phillip Labuschagne <philliplab@gmail.com>
2018-10-24 20:07:17 +0200

    many changes: add sd_hd into report; final changes to make homology report work with many pops

--- basic_homolo_based_fitness/build.sh
+++ basic_homolo_based_fitness/build.sh
@@ -1,4 +1,4 @@
 rm _main.Rmd
-R -e "rmarkdown::render('basic_homolo_based_fitness.Rmd', output_dir = '/home/phillipl/projects/quasispecies_sim_reports/builds/basic_homolo_based_fitness')"
+R -q -e "rmarkdown::render('/home/phillipl/projects/quasispecies_sim_reports/repo/quasispecies_sim_reports/basic_homolo_based_fitness/basic_homolo_based_fitness.Rmd', output_dir = '/home/phillipl/projects/quasispecies_sim_reports/builds/basic_homolo_based_fitness')"
 firefox /home/phillipl/projects/quasispecies_sim_reports/builds/basic_homolo_based_fitness/basic_homolo_based_fitness.html
 rm -r _bookdown_files
