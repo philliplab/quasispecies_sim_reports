f3ca8d65452e7336b222ef63775fe85671bd4a2f
Phillip Labuschagne <philliplab@gmail.com>
2018-10-24 13:44:45 +0200

    Get basics of first_useful_comparison working with many_pops

--- first_useful_comparison/build.sh
+++ first_useful_comparison/build.sh
@@ -1,4 +1,4 @@
 rm _main.Rmd
-R -e "rmarkdown::render('first_useful_comparison.Rmd', output_dir = '/home/phillipl/projects/quasispecies_sim_reports/builds/first_useful_comparison')"
+R -q -e "setwd('/home/phillipl/projects/quasispecies_sim_reports/repo/quasispecies_sim_reports/first_useful_comparison'); rmarkdown::render('first_useful_comparison.Rmd', output_dir = '/home/phillipl/projects/quasispecies_sim_reports/builds/first_useful_comparison')"
 firefox /home/phillipl/projects/quasispecies_sim_reports/builds/first_useful_comparison/first_useful_comparison.html
 rm -r _bookdown_files
